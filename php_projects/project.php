907ce5a6d4f00b1aa260a9c1ee94c743f711d06a
islkanit <Koufi613@gmail.com>
2025-03-23 17:54:15 +0100

    Create project.php

---
+++ php_projects/project.php
@@ -0,0 +1,50 @@
+<?php
+    echo "i love coding!";
+    echo "its really good";
+  
+
+
+
+
+
+echo "playstd";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>order pizza</button>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
